82992a0b3b11722dc129e333550e5b35aea6aefd
⪻乂  ᑌ ℞ Ꭵ ᙭ ᙭ ꗃ 乂⪼⇝⎝ツ⎠﹃Ό <155140402+URIXXxNAITIK@users.noreply.github.com>
2023-12-31 12:32:28 +0530

    Update bot.py

--- AltBots/modules/bot.py
+++ AltBots/modules/bot.py
@@ -21,10 +21,10 @@ from datetime import datetime
 async def ping(e):
     if e.sender_id in SUDO_USERS:
         start = datetime.now()
-        altron = await e.reply(f"» 𝐇𝐀𝐂𝐊𝐄𝐑 𝐗 𝐒𝐏𝐀𝐌 𝐁𝐎𝐓\n🔸𝐏𝐨𝐰𝐞𝐫𝐞𝐝 𝐁𝐲 - @Legend_of_all_groups\n🔸𝐀 𝐏𝐚𝐫𝐭 𝐎𝐟 - @O_P_Hacker")
+        altron = await e.reply(f"» 𝐔𝐑𝐈𝐗𝐗 𝐗 𝐒𝐏𝐀𝐌 𝐁𝐎𝐓\n🔸𝐏𝐨𝐰𝐞𝐫𝐞𝐝 𝐁𝐲 - @URIXX_X_0\n🔸𝐀 𝐏𝐚𝐫𝐭 𝐎𝐟 - @URIXX_X_0")
         end = datetime.now()
         mp = (end - start).microseconds / 1000
-        await altron.edit(f"𝐇𝐀𝐂𝐊𝐄𝐑 𝐗 𝐒𝐏𝐀𝐌 𝐁𝐎𝐓\n🔸𝐏𝐨𝐰𝐞𝐫𝐞𝐝 𝐁𝐲 - @Legend_of_all_groups\n🔸𝐀 𝐏𝐚𝐫𝐭 𝐎𝐟 - @O_P_Hacker\n» `{mp} ᴍꜱ`")
+        await altron.edit(f"𝐔𝐑𝐈𝐗𝐗 𝐗 𝐒𝐏𝐀𝐌 𝐁𝐎𝐓\n🔸𝐏𝐨𝐰𝐞𝐫𝐞𝐝 𝐁𝐲 - @URIXX_X_0\n🔸𝐀 𝐏𝐚𝐫𝐭 𝐎𝐟 - @URIXX_X_0\n» `{mp} ᴍꜱ`")
 
 
 @X1.on(events.NewMessage(incoming=True, pattern=r"\%sreboot(?: |$)(.*)" % hl))
